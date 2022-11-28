a8a737ce15c5bdb30e430ef9bb696a67a293e649
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-27 22:22:06 -0300

    add theme

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,26 +1,29 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    html,
-    body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        background: url('/static/background.png');
-        background-position: 60%;
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
+    ${({theme}) => css`
+        html,
+        body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+                Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+            background: url('/static/background.png');
+            background-position: 60%;
+            background-repeat: no-repeat;
+            background-size: cover;
+            height:100%;
+            color: ${theme.colors.purple700};
+        }
 
-    a {
-        color: inherit;
-    }
+        a {
+            color: inherit;
+        }
 
-    * {
-        box-sizing: border-box;
-    }
-
+        * {
+            box-sizing: border-box;
+        }
+    `}
 `
 
 export default GlobalStyle
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+    colors: {
+        purple300:'#4f46e5',  
+        purple500:'#4338ca',  
+        purple700:'#373b64',  
+        text:'#111827',  
+        textWhite:'#e5e7eb',  
+    }
+  } as const
+  
+  
