d73cff999c038f814e2591683176c8be7230ee11
JoaoVitorLima242 <jlima0500@gmail.com>
2023-01-18 17:08:14 -0300

    Only use post method in create route

--- src/pages/api/createCoffeeStore.ts
+++ src/pages/api/createCoffeeStore.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { table } from '../../services/airtable'
 
-
 const createCoffeeStore = (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method !== 'POST') {
+        res.status(405).json({message: 'Only POST methods for this route'})
+        return
+    }
 
     res.status(200).json({message: 'teste'})
 }
